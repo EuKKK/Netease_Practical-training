9030975762b88d4d600a1192dd861a17852b407e
俊炜 郭 <734989408@qq.com>
2020-05-29 14:58:31 +0800

    增加了碰撞检测和地图格子生成

---
+++ 2DGame/Assets/scripts/mapCreater.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class mapCreater : MonoBehaviour
+{
+    public GameObject map;
+    public GameObject[] maps;
+    public int rows;
+    public int cols;
+    public float width;                     //格子宽度
+    private const float rates = 1.73205f;
+    public bool is_align_center=true;       //是否居中
+    public Vector3 top_left_coordinate;     //左上角坐标，居中的话自己计算不需要提供
+    // Start is called before the first frame update
+    void Start()
+    {
+        maps = new GameObject[rows*cols];
+        map = Resources.Load("Maps") as GameObject;
+
+
+        if (is_align_center)
+        { 
+            top_left_coordinate.x = -width * (cols % 2 == 0 ? (cols / 2-0.5f) : (cols / 2 ));
+            top_left_coordinate.y = (rates/2)*width * (rows % 2 == 0 ? (rows / 2 - 0.5f) : (rows / 2 ));
+        }
+        
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                if(i%2==0)
+                    maps[i*j+j]=Instantiate(map, new Vector3(top_left_coordinate.x+j*width, top_left_coordinate.y - i * width * rates / 2, 0), Quaternion.identity);
+                else
+                    maps[i * j + j] = Instantiate(map, new Vector3(top_left_coordinate.x+ j * width+0.5f , top_left_coordinate.y - i * width * rates / 2, 0), Quaternion.identity);
+            }
+        }
+
+
+        //maps[0] = Instantiate(map);
+        //maps[1] = Instantiate(map, new Vector3( -0.5F, 0, 0), Quaternion.identity);
+        //maps[2] = Instantiate(map, new Vector3(0.5F, 0, 0), Quaternion.identity);
+        //maps[3] = Instantiate(map, new Vector3(1.0F, 0, 0), Quaternion.identity);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2DGame/Assets/scripts/test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public Color color;
+    private float r;
+    private float b;
+    private float g;
+    // Start is called before the first frame update
+    void Start()
+    {
+        color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        r = 1.0f;
+        b = 0.5f;
+        g = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        SpriteRenderer sprite = collider.gameObject.GetComponent<SpriteRenderer>();
+        color.r = Mathf.Sin(r * Time.time);
+        color.g = Mathf.Sin(2*g * Time.time);
+        color.b = Mathf.Sin(b * Time.time);
+        sprite.color = color;
+    }
+}
